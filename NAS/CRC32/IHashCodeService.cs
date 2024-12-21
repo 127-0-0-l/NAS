cfa6a0af98ce282ba8623a826ed766f553c6d6f4
maks <borisovets.maksim@gmail.com>
2024-12-22 01:56:19 +0300

    add crc32

---
+++ NAS/CRC32/IHashCodeService.cs
@@ -0,0 +1,9 @@
+﻿namespace HashCodeService
+{
+    public interface IHashCodeService
+    {
+        uint GetHashCode(object data);
+        uint GetHashCode(byte[] data);
+        uint GetFileHashCode(string filePath);
+    }
+}
