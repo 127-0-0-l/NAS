5978dcad4ef536e85367b6ba6dfa227b7a5fd789
maks <borisovets.maksim@gmail.com>
2024-12-30 11:03:41 +0300

    add logging

---
+++ NAS/NAS.Utils/Helpers/LoggingInterceptor.cs
@@ -0,0 +1,29 @@
+﻿using Ninject.Extensions.Interception;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace NAS.Utils.Helpers
+{
+    public class LoggingInterceptor: IInterceptor
+    {
+        public void Intercept(IInvocation invocation)
+        {
+            MethodInfo methodInfo = invocation.Request.Method;
+            IEnumerable<string> parameterNames = methodInfo.GetParameters().Select(x => x.Name);
+            LoggingHelper.LogMethodEntering(methodInfo.DeclaringType, methodInfo.Name, parameterNames, invocation.Request.Arguments);
+
+            try
+            {
+                invocation.Proceed();
+                LoggingHelper.LogMethodLeaving(methodInfo.DeclaringType, methodInfo.Name);
+            }
+            catch (Exception exception)
+            {
+                LoggingHelper.LogMethodException(methodInfo.DeclaringType, exception);
+                throw;
+            }
+        }
+    }
+}
---
+++ NAS/NAS.Web/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,16 @@
+﻿using Ninject.Modules;
+using Ninject.Extensions.Interception.Infrastructure.Language;
+using NAS.Utils.Helpers;
+using Ninject.Web.Common;
+using System.Web;
+
+namespace NAS.Web.App_Start
+{
+    internal class NinjectWebCommon : NinjectModule
+    {
+        public override void Load()
+        {
+            //Bind<object>().To<object>().Intercept().With<LoggingInterceptor>();
+        }
+    }
+}
